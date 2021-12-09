afa2dc0a68e52615326f849c192c4d15c0d9a5b6
Martin Smits <msmits@ommos.net>
2021-12-09 12:31:23 +0100

    Updated template.c to separate out the reading of the data Started Implementation of a generic dynamic array

--- common/darray.h
+++ common/darray.h
@@ -1,28 +1,49 @@
-// dynamic array stuff
-typedef struct darray_info {
-    int count;
-    int element_size;
-} darray_info;
+//
+// Header file and implementation for dynamic arrays
+//
 
-int capacity(void *data) {
+// External interface provides only functions to push elements
+// at the end of the array
+
+#define darray_push(a, el) _darray_push(&(a), (&el), sizeof(el))
+_darray_push(void **array, void *element, int element_size);
+darray_count(void *array);
+darray_free(void *array);
+
+// dynamic array stuff
+#ifdef _DARRAY_IMPLEMENTATION
+
+// Structure to hold information about the currently allocated data
+// This structure is located before the the actual data that is
+// exposed to the user
+typedef struct _darray_info {
+    int capacity;       // the potential number of elements
+    int count;          // the actual current element count
+    int element_size;   // the size of one element in bytes
+} _darray_info;
+
+int _darray_capacity(void *data) {
     int result = 0;
 
     if (data != NULL) {
-        result = ((darray_info *)data)[-1].count;
+        result = ((_darray_info *)data)[-1].capacity;
     }
 
     return result;
 }
 
-void *resize(void *data, int count, int element_size) {
-    darray_info *result = NULL;
+void *resize(void *array, int capacity, int element_size) {
+    _darray_info *result = NULL;
 
     result = malloc(sizeof(int) + count * element_size);
-    result[0].count = count;
+    result[0].capacity = capacity;
     result[0].element_size = element_size;
 
     // TODO: copy the old data
 
     return (void *)result;
 }
+#endif
 
+#ifdef _DARRAY_TEST
+#endif
--- template.c
+++ template.c
@@ -1,11 +1,44 @@
 
+#include <assert.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 #define array_count(a) (sizeof(a)/sizeof(a[0]))
-#define LINE_LENGTH 80
+#define LINE_LENGTH 1024
+
+// TODO: Global data information
+
+// Function to read all input data to memory
+void readData(char *fname) {
+    FILE *fin = fopen(fname, "r");
+    if (fin == NULL) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    int line_count = 0;
+    char line[LINE_LENGTH];
+    while (fgets(line, LINE_LENGTH, fin) != NULL) {
+        // strip line ending
+        if (strlen(line) != 0) line[strlen(line) - 1] = 0;
+
+        int value = 0;
+        if (sscanf(line, "%d", &value) == 1) {
+            // TODO: parse the data
+        } else if (errno != 0) {
+            perror("sscanf");
+        } else {
+            fprintf(stderr, "Unexpected input format '%s'.\n", line);
+        }
+
+        line_count++;
+    }
+
+    printf("lines = %d\n", line_count);
+    fclose(fin);
+}
 
 int main(int argc, char *argv[]) {
     char *fname = "input.txt";
@@ -15,28 +48,9 @@ int main(int argc, char *argv[]) {
         fname = argv[1];
     }
 
-    char line[LINE_LENGTH];
-    FILE *fin = fopen(fname, "r");
-    if (fin == NULL) {
-        perror("fopen");
-        exit(EXIT_FAILURE);
-    }
+    readData(fname);
 
-    int count = 0;
-    while (fgets(line, LINE_LENGTH, fin) != NULL) {
-        if (strlen(line) != 0) line[strlen(line) - 1] = 0;
-        int value = 0;
-        if (sscanf(line, "%d", &value) == 1) {
-        } else if (errno != 0) {
-            perror("sscanf");
-        } else {
-            fprintf(stderr, "Unexpected input format '%s'.\n", line);
-        }
-        count++;
-    }
-
-    printf("lines = %d\n", count);
-    fclose(fin);
+    // TODO: implement algorithm
 
     return EXIT_SUCCESS;
 }
